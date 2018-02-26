70c6ae0a75437d8c8f173905fa5f856b29d227c7
ahutsunshine <ahutsunshine@gmail.com>
2018-02-26 20:07:50 +0800

    fix mysql start

--- svc_msr/prep/init/mysql-init.sh
+++ svc_msr/prep/init/mysql-init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-find /var/lib/mysql -type f -exec touch {} \; && service mysql start
+RUN apt-get update && apt-get install -y mysql-server-5.5
+RUN find /var/lib/mysql -type f -exec touch {} \; && service mysql start
 
 mysql -u root < quartz-init.sql
 mysql -u griffin -p123456 -D quartz < quartz-table-init.sql
